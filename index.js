b6477c4fe82ed824dad7cee0a3e3fd30f99c4c07
Montso Moalosi <montsoernestmoalosi@gmail.com>
2023-11-14 00:17:30 +0200

    Primary Color Theme Customization using JavaScript

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const theme = document.querySelector('#theme');
 const themeModal = document.querySelector('.customize-theme');
 const fontSizes = document.querySelectorAll('.choose-size span'); 
 var root = document.querySelector(':root');
+const colorPalette = document.querySelectorAll('.choose-color span'); 
 
 // Remove active class from all menu items
 const changeActiveItem = () =>{
@@ -119,3 +120,31 @@ fontSizes.forEach(size => {
         document.querySelector('html').style.fontSize = fontSize;
      }); 
 })
+
+// remove active class from colors
+const changeActiveColorClass = () => {
+    colorPalette.forEach(colorPicker =>{
+        colorPicker.classList.remove('active');
+    })
+}
+
+// change primary colors
+colorPalette.forEach(color => {
+    color.addEventListener('click', () =>{
+        let primaryHue;
+        changeActiveColorClass();
+        if(color.classList.contains('color-1')){
+            primaryHue = 252;
+        }else if(color.classList.contains('color-2')){
+            primaryHue = 52;
+        }else if(color.classList.contains('color-3')){
+            primaryHue = 352;
+        }else if(color.classList.contains('color-4')){
+            primaryHue = 152;
+        }else if(color.classList.contains('color-5')){
+            primaryHue = 202;
+        }
+        color.classList.add('active');
+        root.style.setProperty('--primary-color-hue', primaryHue);
+    });
+});
